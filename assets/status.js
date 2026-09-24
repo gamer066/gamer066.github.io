29706945b8090f6deea07ffa3d83cbde55c05bbb
gamer066 <gamer066@users.noreply.github.com>
2026-09-24 09:03:23 +0400

    Update dashboard

--- assets/status.js
+++ assets/status.js
@@ -1,2 +1,2 @@
 /* Written automatically every time the site is published. Do not edit by hand. */
-window.SITE_STATUS = {"bots": 7, "openTrades": 11, "mode": "Practice", "updated": "2026-09-24T02:03:24Z"};
+window.SITE_STATUS = {"bots": 7, "openTrades": 12, "mode": "Practice", "updated": "2026-09-24T05:03:22Z"};
